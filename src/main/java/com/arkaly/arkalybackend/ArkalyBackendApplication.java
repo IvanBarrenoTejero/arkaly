87819c574f3aafbefa9afa37d74c0f8c61613007
IvanBarrenoTejero <ivanbartej11@gmail.com>
2026-03-20 10:55:53 +0100

    Commit inicial

---
+++ src/main/java/com/arkaly/arkalybackend/ArkalyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.arkaly.arkalybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkalyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArkalyBackendApplication.class, args);
+    }
+
+}
